f7072f4ab69e27d85b93169c7f750039c339c01d
Anirudh Jangid <133129946+Codewithgitanirudh@users.noreply.github.com>
2024-02-27 19:09:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ function App() {
       <div id='hero'>
 
         <div id='img1'>
-          <img src="img\asserts\1.png" alt="" />
+          <img src="img\1.png" alt="" />
         </div>
 
         <div id='main'>
-          <img src=".\img\asserts\logo.png" />
+          <img src="img\logo.png" />
 
           <h1>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</h1>
 
           <h2> C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</h2>
           <h2>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</h2>
 
-          <img src="img\asserts\2.png" alt="" id='grp' />
+          <img src="img\2.png" alt="" id='grp' />
 
           <h2 id='h'>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</h2>
         </div>
